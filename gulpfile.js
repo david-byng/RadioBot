062d331f7ca3aa9ac5336233cbf885f38f8084f0
Eugene Svidko <eugene@byng.co>
2015-05-09 18:55:00 +0100

    Adding the main widget

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var child_process = require("child_process");
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var exec = require("child_process").exec;
+var exec = require('child_process').exec;
+var plumber = require('gulp-plumber');
+var neat = require('node-neat').includePaths;
 
 var paths = {
   js: ['./www/js/**/*.js']
@@ -12,12 +14,30 @@ var paths = {
 
 gulp.task('default', ['sass']);
 
+gulp.task("sass", function() {
+    gulp
+        .src([
+            "www/sass/*.scss"
+        ])
+        .pipe(plumber())
+        .pipe(sass({
+            includePaths: ['styles'].concat(neat)
+        }))
+        .pipe(gulp.dest("www/compiled/css/"))
+    ;
+});
+
 gulp.task("watch", function() {
     var watch = require("gulp-watch");
 
     watch(paths.js, function() {
         gulp.start("scripts");
     });
+
+    watch("www/sass/**/*", function() {
+        gulp.start("build-css");
+    });
+
 });
 
 gulp.task("jshint", function() {
